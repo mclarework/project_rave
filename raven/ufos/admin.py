0dd83c053a210c1b8c5b7e204bdd0b21a9cdb55a
Mike Clare <mclarework@gmail.com>
2020-08-27 12:23:45 +0100

    initial models setup complete

--- raven/ufos/admin.py
+++ raven/ufos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Event
+from .models import Location
+
+admin.site.register(Event)
+admin.site.register(Location)
+
 
 # Register your models here.
--- raven/ufos/models.py
+++ raven/ufos/models.py
@@ -1,3 +1,25 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
+class Event(models.Model):
+    event_text = models.CharField(max_length=200)
+    event_description = models.CharField(max_length=200, blank=True, default="")
+    # event_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.event_text
+
+    @property
+    def last_update(self):
+        return self.updated_at >= timezone.now() - datetime.timedelta(days=7)
+
+class Location(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    location_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.location_text
 
